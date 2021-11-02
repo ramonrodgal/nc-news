9210e768f739828589c5b37c838df1e339aec026
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-02 09:15:10 +0000

    Adds try/catch block to getTopics controller function

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,10 @@
 const { fetchTopics } = require('../models/topics.models');
 
 exports.getTopics = async (req, res, next) => {
-  const topics = await fetchTopics();
-
-  res.status(200).send({ topics });
+  try {
+    const topics = await fetchTopics();
+    res.status(200).send({ topics });
+  } catch (err) {
+    next(err);
+  }
 };
