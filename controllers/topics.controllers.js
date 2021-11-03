034bbff4bce4e14958a468de3a023506ffe64809
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-03 16:50:44 +0000

    Creates and tests POST methpd in /api/topics to respond with 200 and the added topic

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,20 @@
-const { fetchTopics } = require('../models/topics.models');
+const { fetchTopics, insertTopic } = require('../models/topics.models');
 
 exports.getTopics = async (req, res, next) => {
   try {
     const topics = await fetchTopics();
     res.status(200).send({ topics });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postTopic = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const topic = await insertTopic(body);
+    res.status(201).send({ topic });
   } catch (err) {
     next(err);
   }
--- models/topics.models.js
+++ models/topics.models.js
@@ -4,3 +4,19 @@ exports.fetchTopics = async () => {
   const { rows: topics } = await db.query(`SELECT * from topics;`);
   return topics;
 };
+
+exports.insertTopic = async (body) => {
+  const { slug, description } = body;
+
+  const queryString = `
+    INSERT INTO topics
+      (slug, description)
+    VALUES
+      ($1, $2)
+    RETURNING *`;
+  const queryParams = [slug, description];
+
+  const { rows } = await db.query(queryString, queryParams);
+
+  return rows[0];
+};
